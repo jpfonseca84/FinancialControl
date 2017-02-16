e9cb0ce1eca10d2c560e1c2c91c975949af1a90f
jpfonseca84 <jpfonseca@gmail.com>
2017-02-16 13:29:13 -0500

    changing the objective of the function. Now it will bring the complete category summary from a specific date. Only

--- totalextract-F.R
+++ totalextract-F.R
@@ -1,107 +1,48 @@
 #Sum of expenses per month and year
 totalextract <-
-      function(f.month = NULL,
-               f.year = NULL,
-               f.category = NULL,
+      function(f.date = NULL,
                f.numofcat=NULL,
-               f.opencat=F,
-               f.day=1,
                f.fileaddress= "C:\\Users\\jpfon\\Google Drive\\Pessoal\\Documentos\\FinancialControl\\totalcostsfile"){
             
+#Define the oldest date to search
+           
+            #If f.date is blank
+            if(is.null(f.date)){
+            f.date<-today()      
+            day(f.date)<-1
+            }
+            #if f.date is not blank
+            ##Coherce f.date as a Date class Object
+            if(!is.null(f.date)){
+                  f.date<-as.Date(f.date,'%Y %m %d')
+                  day(f.date)<-1
+            }
+            
+#ORGANIZE THE FILE---------------- 
             #obtain the file Totalcostfiles
             tempframe<-read.csv(file=f.fileaddress)
             #turn negative in positive
             tempframe$value<-tempframe$value*-1
+            #Transform the date column in a date vector
+            tempframe$date<-as.Date(tempframe$date)
             
-            #Transform the columns date to a Month and Year to 
-            #work with previous dataset
-            date.vector<-as.vector(read.csv(file=f.fileaddress)[,4])
-            month.vector<-as.vector(as.numeric(substr(date.vector,6,7)))
-            year.vector<-as.vector(as.numeric(substr(date.vector,1,4)))
-            
-            tempframe<-tempframe[,-4]
-            tempframe<-data.frame(cbind(tempframe,
-                                        month=month.vector,
-                                        year=year.vector))
-            
-            ##Define the month and year -----------
-            
-            #---------------------------------
-            #turn negative in positive
-            #InputErrors
-            if(is.null(category)&f.opencat==T){
-                  stop("To open a category, please provide it in 
-                       \"category\"")
-            }
-            
-            #Provided MONTH AND YEAR -------------------------------------
-      if (!is.null(month) & !is.null(f.year)) {
-                  if (is.null(category)) {
-                        #NO f.category
-                        tempframe <-
-                              tempframe[tempframe$month == f.month &
-                                               tempframe$year == f.year, ]
-                  }
-                  else{
-                        #WITH f.category
-                        tempframe <-
-                              tempframe[tempframe$month == f.month &
-                                               tempframe$year == f.year &
-                                               tempframe$category == f.category,]
-                  }
-            }
-            
-            #ONLY f.year is provided
-            if (is.null(f.month) & !is.null(f.year)) {
-                  if (is.null(f.category)) {
-                        #NO f.category
-                        tempframe <-
-                              tempframe[tempframe$year == f.year, ]
-                  } else{
-                        #WITH f.category
-                        tempframe <-
-                              tempframe[tempframe$year == f.year &
-                                               tempframe$category == f.category, ]
-                  }
-            }
+            #create the specific tempframe for the answer
+            tempframe<-tempframe[tempframe$date>=as.Date(f.date),]
+            #adjusting levels of tempframe
+            tempframe$category<-droplevels.data.frame(tempframe)$category
             
             
-            #only f.month provided
-            if (is.null(f.year) & !is.null(f.month)) {
-                  if (is.null(f.category)) {
-                        #NO f.category
-                        tempframe <-
-                              tempframe[tempframe$month == f.month, ]
-                  } else{
-                        #WITH f.category
-                        tempframe <-
-                              tempframe[tempframe$month == f.month &
-                                               tempframe$category == f.category, ]
-                  }
-            }
-            ####Return of error ####
+####Return of error --------------------
             if (nrow(tempframe) == 0) {
                   stop("No information in the selected period")
             }
-            
-            #adjusting levels of tempframe
-            tempframe$category<-droplevels.data.frame(tempframe)$category
-            #defining number of categories for f.numofcat NULL
-            if(is.null(f.numofcat)){
-                  f.numofcat<-length(levels(tempframe$category))
-            }
-            
-            ####RETURN OF THE PLOT BASED IN f.opencat####
-                  if (f.opencat==F) {
+#Return Answer ------------------
+            else{ 
                   return(barplot(sort(
                         tapply(tempframe$value,
-                               tempframe$category, sum),
-                  decreasing=T)[1:f.numofcat]))
-            } else{
-                  return(barplot(sort(
-                        tapply(tempframe$value[tempframe$category==f.category],
-                               tempframe$type[tempframe$category==f.category],
+                               tempframe$category,
                                sum),
-                        decreasing=T)))
-            }
+                        decreasing=T))[1:numofcat])
+           }
+            
       }
