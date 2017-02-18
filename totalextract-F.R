9e77f18ccb6d50eedde06a8116b047a361c5b9b8
jpfonseca84 <jpfonseca@gmail.com>
2017-02-17 22:19:14 -0500

    File updated to be able to report a date.
    Still need to add a segment to evaluate only an interval of date. As for now, it only does the query starting on a date.

--- totalextract-F.R
+++ totalextract-F.R
@@ -1,48 +1,50 @@
-#Sum of expenses per month and year
-totalextract <-
-      function(f.date = NULL,
-               f.numofcat=NULL,
-               f.fileaddress= "C:\\Users\\jpfon\\Google Drive\\Pessoal\\Documentos\\FinancialControl\\totalcostsfile"){
-            
-#Define the oldest date to search
-           
-            #If f.date is blank
-            if(is.null(f.date)){
-            f.date<-today()      
-            day(f.date)<-1
-            }
-            #if f.date is not blank
-            ##Coherce f.date as a Date class Object
-            if(!is.null(f.date)){
-                  f.date<-as.Date(f.date,'%Y %m %d')
-                  day(f.date)<-1
-            }
-            
-#ORGANIZE THE FILE---------------- 
-            #obtain the file Totalcostfiles
-            tempframe<-read.csv(file=f.fileaddress)
-            #turn negative in positive
-            tempframe$value<-tempframe$value*-1
-            #Transform the date column in a date vector
-            tempframe$date<-as.Date(tempframe$date)
-            
-            #create the specific tempframe for the answer
-            tempframe<-tempframe[tempframe$date>=as.Date(f.date),]
-            #adjusting levels of tempframe
-            tempframe$category<-droplevels.data.frame(tempframe)$category
-            
-            
-####Return of error --------------------
-            if (nrow(tempframe) == 0) {
-                  stop("No information in the selected period")
-            }
-#Return Answer ------------------
-            else{ 
-                  return(barplot(sort(
-                        tapply(tempframe$value,
-                               tempframe$category,
-                               sum),
-                        decreasing=T))[1:numofcat])
-           }
-            
+#Sum of expenses per category in the period
+totalextract <- function(f.date = today(),
+                         f.numofcat = NULL,
+                         f.fileaddress = "C:\\Users\\jpfon\\Google Drive\\Pessoal\\Documentos\\FinancialControl\\totalcostsfile") {
+      
+      ##Coherce f.date as a Date class object in case it's a text
+      if(is.character(f.date)) {
+            f.date <- ymd(f.date)
       }
+      
+      #Define the oldest date to search
+      day(f.date) <- 1
+      
+      #ORGANIZE THE FILE----------------
+      
+      #obtain the file Totalcostfiles
+      tempframe <- read.csv(file = f.fileaddress)
+      
+      #turn negative in positive
+      tempframe$value <- tempframe$value * -1
+      
+      #Transform the date column in a date vector
+      tempframe$date <- as.Date(tempframe$date)
+      
+      #create the specific tempframe for the answer
+      tempframe <- tempframe[tempframe$date >= f.date, ]
+      
+      #adjusting levels of tempframe
+      tempframe$category <-
+            droplevels.data.frame(tempframe)$category
+      
+      #define the number of f.numofcat
+      if (is.null(f.numofcat)) {
+            f.numofcat <- length(levels(tempframe$category))
+      }
+      
+      ####Return of error --------------------
+      if (nrow(tempframe) == 0) {
+            stop("No information in the selected period")
+      }
+      #Return Answer ------------------
+      else{
+            return(barplot(sort(
+                  tapply(tempframe$value,
+                         tempframe$category,
+                         sum),
+                  decreasing = T)[1:f.numofcat]))
+      }
+      
+}
