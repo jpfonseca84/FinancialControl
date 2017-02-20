5c8c413f034a5f480a8ec4df1a863e4ab481cbb9
jpfonseca84 <jpfonseca@gmail.com>
2017-02-20 10:12:59 -0500

    Added a color to the Graphplotter

--- totalextract-F.R
+++ totalextract-F.R
@@ -3,11 +3,11 @@ totalextract <- function(f.date.start = today(), #As "%Y %m %d"
                          f.date.stop = today(),  #As "%Y %m %d"
                          f.numofcat = NULL,
                          f.fileaddress = "C:\\Users\\jpfon\\Google Drive\\Pessoal\\Documentos\\FinancialControl\\totalcostsfile") {
-##Coherce f.dates as a Date class object
+##Coherce f.dates as a Date class object ----
             f.date.start <- ymd(f.date.start)
             f.date.stop <- ymd(f.date.stop)
             
-##Input Error
+##Input Error -----
       if (f.date.stop < f.date.start) {
             stop("Stop date prior to Start date. Please correct")
       }
@@ -24,22 +24,7 @@ totalextract <- function(f.date.start = today(), #As "%Y %m %d"
             day(f.date.stop) <- 1
             month(f.date.stop) <-month(f.date.stop) + 1
             day(f.date.stop) <-day(f.date.stop) - 1#point it to monthend
-            
-            
-            #      day(f.date.start) <- 1#Define the initial start date to search
-#      
-##      if (month(f.date.stop) != 12) {
- #           #proceeding if not december
- #           month(f.date.stop) <- month(f.date.stop) + 1
- #           day(f.date.stop) <- 1 #define it as 1
- #           day(f.date.stop) <-
- #                 day(f.date.stop) - 1#point it to monthend
- #     }else{
- #           year(f.date.stop) <- year(f.date.stop)+1
- #           month(f.date.stop) <- 1
- #           day(f.date.stop) <- 1
- #           day(f.date.stop) <-day(f.date.stop) - 1#point it to monthend
- #     }
+           
       
 #ORGANIZE THE FILE----------------
       
@@ -64,7 +49,7 @@ totalextract <- function(f.date.start = today(), #As "%Y %m %d"
             f.numofcat <- length(levels(tempframe$category))
       }
 
-      #The vector of values to be printed
+#The vector of values to be printed ----
       values.to.print <- sort(tapply(tempframe$value,
                                      tempframe$category,
                                      sum),
@@ -75,12 +60,12 @@ totalextract <- function(f.date.start = today(), #As "%Y %m %d"
       }
 #print the plot ------------------
       theplot <- barplot(values.to.print,
-                         names.arg = names(values.to.print))
+                         names.arg = names(values.to.print),
+                         col=1:length(levels(tempframe$category)))
       legend("topright",
              legend = c(
                    paste("Start ", f.date.start),
-                   paste("Stop ", f.date.stop)
-             ))
+                   paste("Stop ", f.date.stop)))
       
       return(data.frame(total=values.to.print))
 }
