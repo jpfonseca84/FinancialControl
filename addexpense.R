e4669d6676199cb6c86bba6b439f01bcd6b12cea
jpfonseca84 <jpfonseca@gmail.com>
2017-02-19 19:09:49 -0500

    Updated the names of the args in the form

--- addexpense.R
+++ addexpense.R
@@ -1,19 +1,23 @@
 add.expense <-
       function(f.category,
-               f.type = NA,
+               f.type,
                f.value,
                f.place = NA,
                f.date = as.character(Sys.Date()),
                f.fileaddress ="C:\\Users\\jpfon\\Google Drive\\Pessoal\\Documentos\\FinancialControl\\totalcostsfile"){
 
             #Merging original data with input data
-            tempframeA <-read.csv(file = fileaddress)
-            tempframeB <- cbind(category, type, value, date, place)
+            tempframeA <-read.csv(file = f.fileaddress)
+            tempframeB <- cbind(category=f.category, 
+                                type=f.type, 
+                                value=f.value, 
+                                date=f.date, 
+                                place=f.place)
             tempframe <- rbind(tempframeA, tempframeB)
             
             #Saving the new complete data set in the file location
             write.csv(tempframe, 
-                      file = fileaddress,
+                      file = f.fileaddress,
                       row.names = F)
             
             #Return the new data set inputted
